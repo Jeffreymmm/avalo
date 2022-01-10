4f03713fdb3997d88c46ddbd4bd8faf806760d11
Jeffrey <wangjh@chinacscs.com>
2022-01-10 17:45:25 +0800

    f

---
+++ umi/config/config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', component: '@/pages/index' },
+  ],
+  fastRefresh: {},
+  dva: {
+    immer: true,
+    hmr: true,
+  },    
+  antd:{
+      mobile:true,
+  }
+});
---
+++ umi/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import styles from './index.less';
+import { Button } from 'antd-mobile'
+export default function IndexPage() {
+  return (
+    <div>
+      <Button>23</Button>
+      <h1 className={styles.title}>Page index</h1>
+    </div>
+  );
+}
